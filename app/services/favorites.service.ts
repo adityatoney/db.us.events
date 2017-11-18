53ea096e2765911ee74ea786aa50ebc964870eb9
Aditya Toney <Administrator@Adityas-iMac.local>
2017-11-18 10:02:20 -0800

    Adding offline cache for sessions and favorites

--- app/services/favorites.service.ts
+++ app/services/favorites.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from "@angular/core";
+import { Injectable } from "@angular/core";
 
 import * as appModule from "application";
 import * as appSettingsModule from "application-settings";
@@ -8,16 +8,50 @@ import * as typesModule from "utils/types";
 import { IFavouriteSession, ISession } from "../shared/interfaces";
 
 @Injectable()
-export class FavoritesService implements OnInit {
+export class FavoritesService {
 
     public favourites: Array<IFavouriteSession> = [];
 
-    public ngOnInit() { }
+    public constructor() { 
+        try {
+            this.favourites = <Array<IFavouriteSession>>JSON.parse(appSettingsModule.getString('FAVOURITES', '[]'));
+        }
+        catch (error) {
+            console.log('Error while retrieveing favourites from the local cache: ' + error);
+            this.favourites = new Array<IFavouriteSession>();
+            this.updateFavourites();
+        }
+    }
+    
+    public getFavouriteIndex(sessionId: string): number {
+        for (var i = 0; i < this.favourites.length; i++) {
+            if (this.favourites[i].sessionId === sessionId) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-    public addToFavourites(session: ISession) { }
+    public addToFavourites(session: ISession) { 
+        if (this.getFavouriteIndex(session.id) >= 0) {
+            return;
+        }
+        
+        this.favourites.push({sessionId: session.id});
+        this.updateFavourites();
+    }
 
-    public removeFromFavourites(session: ISession) { }
+    public removeFromFavourites(session: ISession) { 
+        var index = this.getFavouriteIndex(session.id);
+        if (index >= 0) {
+            this.favourites.splice(index, 1);
+            this.updateFavourites();
+        }
+    }
 
-    public updateFavourites() { }
-
+    public updateFavourites() { 
+        var updatedFavList = JSON.stringify(this.favourites);
+        console.log('Updating favourites: ' + updatedFavList);
+        appSettingsModule.setString('FAVOURITES', updatedFavList);
+    }
 }
