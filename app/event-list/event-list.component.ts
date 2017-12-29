3caf0d78bcd09293bd918f8b669b879e967acbcd
paravp <paravhpatel@gmail.com>
2017-12-29 12:56:24 -0500

    added event-list and static GP 2018 event

--- app/event-list/event-list.component.ts
+++ app/event-list/event-list.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { DrawerTransitionBase, SlideInOnTopTransition } from "nativescript-pro-ui/sidedrawer";
 import { RadSideDrawerComponent } from "nativescript-pro-ui/sidedrawer/angular";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
     selector: "event-list",
     moduleId: module.id,
-    templateUrl: "./event-list.component.html"
+    templateUrl: "./event-list.component.html",
+    styleUrls: ["event-list.component.scss"]
 })
 export class EventListComponent implements OnInit {
+    constructor(
+        private routerExtensions: RouterExtensions
+    ){
+        
+    }
 
     @ViewChild("drawer") public drawerComponent: RadSideDrawerComponent;
 
@@ -23,5 +30,9 @@ export class EventListComponent implements OnInit {
 
     public onDrawerButtonTap(): void {
         this.drawerComponent.sideDrawer.showDrawer();
+    }
+    public selectEvent() {
+        let link = ['/sessions/1'];
+        this.routerExtensions.navigate(link);
     }
 }
