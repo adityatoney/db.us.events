8eef917323cc9359db27c0fe3dbad2f132827b2a
developknowledge <36106390+developknowledge@users.noreply.github.com>
2018-04-08 13:15:19 -0400

    Tab view test (#25)
    * swapped event-info to event-list page
    
    * added event-list and static GP 2018 event
    
    * styling, rm comments, and floorplan route from session-details
    
    * add back home icon for floorplan-page
    
    * platform specific css for main app and sessions, rm some icons for Phase1
    
    * updated ios drawer-menu icon, event-list styling, and other minor styling
    
    * tabview (bottom-navigation) added for event-info page
    
    * rm extra main schedule
    
    * updated android icon images for tab view

--- app/general-information/general-information.component.ts
+++ app/general-information/general-information.component.ts
@@ -5,6 +5,8 @@ import { ScrollEventData } from "ui/scroll-view";
 import { Page } from "ui/page";
 import { Image } from "ui/image";
 import { FlexboxLayout } from "ui/layouts/flexbox-layout";
+import { NativeScriptRouterModule, RouterExtensions } from "nativescript-angular/router";
+import { BottomNavigation, BottomNavigationTab, OnTabSelectedEventData } from 'nativescript-bottom-navigation';
 
 @Component({
     selector: "general-information",
@@ -25,10 +27,18 @@ export class GeneralInformationComponent implements OnInit {
     public image: Image;
     public flex: FlexboxLayout;
 
+    //Tab-View (Bottom-Navigation) Reference: https://market.nativescript.org/plugins/nativescript-bottom-navigation
+    public tabs: BottomNavigationTab[] = [
+        new BottomNavigationTab('Event Info', 'ic_info_black'),
+        new BottomNavigationTab('Home', 'ic_home_black'),
+        new BottomNavigationTab('Sessions', 'ic_view_list_black'),
+        new BottomNavigationTab('Location', 'ic_location_on_black')
+    ];
+
     @ViewChild("img") img: ElementRef;
     @ViewChild("content") content: ElementRef;
-
-    constructor(public page: Page) {
+    
+    constructor(public page: Page, private routerExtensions: RouterExtensions) {
         this.page.backgroundSpanUnderStatusBar = true;
     }
     
@@ -48,9 +58,9 @@ export class GeneralInformationComponent implements OnInit {
         this._sideDrawerTransition = new SlideInOnTopTransition();
         this.imageSrc = "~/images/jacksonville.jpg";
         this.title = "South West Gurupurnima 2018";
-        this.startDate = "Start Date: July 22, 2018";
+        this.startDate = "Start Date: July 22 2018";
         this.endDate = "End Date: July 28, 2018";
-        this.address = "Address: Jacksonville (TBD) ";
+        this.address = "Address: Jacksonville Florida, USA ";
         // this.desc = "Our theme for Gurupurnima 2018 is global unity, and in this spirit, our tagline is YOUnity starts with YOU! As individuals, we can all play a role in making this event a success! " ;
         this.scheduleTitle = "Main Events";
         this.schedule = "Sunday July 22, 2018: Check-In\nMonday July 23, 2018: Gurupurnima starts\nFriday July 27, 2018: Gurupujan\nSaturday July 28, 2018: Gnanvidhi\nSunday July 29, 2018: Check-out after breakfast";
@@ -64,6 +74,27 @@ export class GeneralInformationComponent implements OnInit {
 
     public onDrawerButtonTap(): void {
         this.drawerComponent.sideDrawer.showDrawer();
+    }
+    
+    onBottomNavigationTabSelected(args: OnTabSelectedEventData): void {
+        if(args.newIndex == 0 && args.oldIndex == 0){
+            console.log("do nothing"); //default will be tab 0, dont want to redirect anywhere as they're already on event-info
+        }
+        else{
+            if(args.newIndex == 0){
+                this.routerExtensions.navigate(['/event-info']);
+            }
+            else if(args.newIndex == 1){
+                this.routerExtensions.navigate(['/event-list']);
+            }
+            else if(args.newIndex == 2){
+                this.routerExtensions.navigate(['/sessions/1']);
+            }
+            else if(args.newIndex == 3){
+                this.routerExtensions.navigate(['location']);
+            }
+            
+        }
     }
 }
 
