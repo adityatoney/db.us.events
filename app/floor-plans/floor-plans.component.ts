f0ae5730a5f573d6784d7f16fc3e7e81bd6228ba
developknowledge <hmpatel4532@hotmail.com>
2018-05-02 21:57:53 -0400

    pull floorplan images via API & fix session icon on tab view

--- app/floor-plans/floor-plans.component.ts
+++ app/floor-plans/floor-plans.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { DrawerTransitionBase, SlideInOnTopTransition } from "nativescript-pro-ui/sidedrawer";
 import { RadSideDrawerComponent } from "nativescript-pro-ui/sidedrawer/angular";
 import { NativeScriptRouterModule, RouterExtensions } from "nativescript-angular/router";
+import { IEvent } from "../shared/interfaces";
+import { SessionModel } from "../sessions/shared/session.model";
 
 @Component({
     selector: "floor-plans",
@@ -15,9 +17,13 @@ import { NativeScriptRouterModule, RouterExtensions } from "nativescript-angular
 
 export class FloorPlansComponent implements OnInit {
     sharedData: boolean;
+    public uniqueFloorURLs: Array<string> = []; //Dummy variable to hold all unique url's
+    public uniqueFloor: Array<Object> = []; //Used to iterate over the floorplan and extract the unique url and floor name
+    
     constructor(
         private routerExtensions: RouterExtensions,
-        private _floorplanNavService: FloorPlanNavService
+        private _floorplanNavService: FloorPlanNavService,
+        private _sessionsService: SessionsService
     ){
         this.sharedData = this._floorplanNavService.getData();
     }
@@ -28,6 +34,28 @@ export class FloorPlansComponent implements OnInit {
     ngOnInit(): void {
         this._sideDrawerTransition = new SlideInOnTopTransition();
         this.sharedData = this._floorplanNavService.getData();
+        this._sessionsService.items.subscribe((observer) => {
+            let delay = 0;
+            observer.forEach((value: SessionModel, i: number, array: Array<SessionModel>) => {
+                delay = delay + 500;
+                setTimeout(() => {
+                    value.triggerShow.next(true);
+                }, delay);
+            });
+        });
+        for(let i = 0; i < this._sessionsService.items.value.length; i++){ //Extract unique floorplan URLs
+            if(this.uniqueFloorURLs.indexOf(this._sessionsService.items.value[i].floorPlanImageUrl) === -1){
+                this.uniqueFloorURLs.push(this._sessionsService.items.value[i].floorPlanImageUrl);
+                this.uniqueFloor.push(this._sessionsService.items.value[i]);
+                console.log(this._sessionsService.items.value[i].floorPlanImageUrl);
+            }
+        }
+    }
+
+    public load() {
+        let p = this._sessionsService.loadSessions<IEvent>()
+            .then((newSessions: IEvent) => {
+            });
     }
 
     get sideDrawerTransition(): DrawerTransitionBase { 
